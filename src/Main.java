25a00ba7499f589fb3e325fc9d54031e2a65a77f
Thando Mini <tzone85@gmail.com>
2017-01-12 15:53:46 +0200

    basic usage using the car as an example

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Thando Mini on 1/12/2017.
+ */
+public class Main {
+
+    public static void main (String [] args){
+        Car porsche = new Car();
+        Car merc = new Car();
+
+        porsche.setModel("911");
+        System.out.println("Model is "+ porsche.getModel());
+    }
+}
